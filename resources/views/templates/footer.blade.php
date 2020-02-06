b40c2be5dc6186654cb972641f8ea975cadd698a
unknown <farhan.ff.fauzaan@gmail.com>
2020-02-07 00:42:33 +0700

    perubahan

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -48,6 +48,15 @@
 
   <!-- Page level custom scripts -->
   <script src="{{asset('assets')}}/js/demo/datatables-demo.js"></script>
+  <script src="{{asset('assets')}}/jquery-3.3.1.js"></script>
+  <script src="{{asset('assets')}}/jquery.dataTables.min.js"></script>
+  <script src="{{asset('assets')}}/dataTables.buttons.min.js"></script>
+  <script src="{{asset('assets')}}/buttons.flash.min.js"></script>
+  <script src="{{asset('assets')}}/3.1.3/jszip.min.js"></script>
+  <script src="{{asset('assets')}}/pdfmake.min.js"></script>
+  <script src="{{asset('assets')}}/vfs_fonts.js"></script>
+  <script src="{{asset('assets')}}/buttons.html5.min.js"></script>
+  <script src="{{asset('assets')}}/buttons.print.min.js"></script>
   <script>
     $('#myModal').on('shown.bs.modal', function () {
       $('#myInput').trigger('focus')
@@ -65,13 +74,4 @@
       } );
     } );
   </script>
-  <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-  <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
-  <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.flash.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-  <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
-  <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.print.min.js"></script>
   @stack('script')
--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -12,8 +12,8 @@
   <title>Pujasera</title>
 
   <!-- Custom fonts for this template-->
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
-  <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.1/css/buttons.dataTables.min.css">
+  <link rel="stylesheet" href="{{asset('assets')}}/jquery.dataTables.min.css">
+  <link rel="stylesheet" href="{{asset('assets')}}/buttons.dataTables.min.css">
   <link href="{{ asset('assets') }}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
